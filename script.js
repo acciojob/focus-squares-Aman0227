d78562ce1b61c6b6089b3eedc6e6d1483cc17dad
Aman0227 <amansingh765487@gmail.com>
2025-12-22 22:29:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let sqr = document.querySelectorAll(".square");
+ sqr.forEach((square) => {
+square.addEventListener("mouseenter", () => {
+	square.style.backgroundColor = "coffee";
+						
+	});
+
+square.addEventListener("mouseleave", () =>{
+square.style.backgroundColor = "#E6E6FA"
+});
+ });
